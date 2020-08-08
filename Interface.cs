dc5651a0216103a4876e7b130053b18c7d591150
AmorFatiBA <you@example.com>
2020-08-08 12:17:31 +0300

    first commit

---
+++ Interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace deneme17_Interface
+{
+     class Koleksiyon : IEnumerable
+    {
+        int[] Dizi;
+        public Koleksiyon(int[] dizi)
+        {
+            this.Dizi = dizi;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new ENumaralandirma(this);
+        }
+        class ENumaralandirma : IEnumerator
+        {
+            int indeks;
+            Koleksiyon koleksiyon;
+            public ENumaralandirma(Koleksiyon koleksiyon)
+            {
+                this.koleksiyon = koleksiyon;
+                indeks = -1;
+            }
+            public void Reset()
+            {
+                indeks = -1;
+            }
+            public bool MoveNext()
+            {
+                indeks++;
+                if (indeks < koleksiyon.Dizi.Length)
+                    return true;
+                else
+                    return false;
+            }
+            object IEnumerator.Current
+            {
+                get {return (koleksiyon.Dizi[indeks]);}
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace deneme17_Interface
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] dizi = { 2, 3, 5, 4, 1, 9, 8, 7 };
+            Koleksiyon k = new Koleksiyon(dizi);
+
+            foreach (int i in k)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+    }
+}
